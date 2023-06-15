2291de577e9e7f330c8e599c0449f6705164036e
Adam Kepczynski <adam.kepczynski32@gmail.com>
2023-06-15 18:45:52 +0200

    added platforms in full funcjonality

--- src/js/mario.js
+++ src/js/mario.js
@@ -23,12 +23,10 @@ class Player {
         this.draw();
         this.position.y += this.velocity.y;
         this.position.x += this.velocity.x;
-        if (this.position.y < canvas.height - this.height - this.velocity.y){
+        if (this.position.y < canvas.height - this.height / 2 - this.velocity.y - 30){
             this.velocity.y += gravity;
         }else{
-            this.velocity.y = 0;
-            keys.up.count = 0;
-            
+            this.velocity.y = 0;            
         }
     }
 
@@ -40,10 +38,10 @@ class Player {
 }
 
 class Platform {
-    constructor(){
+    constructor({x, y}){
         this.position = {
-            x: 0,
-            y: 0
+            x,
+            y
         };
         this.width = 200;
         this.height = 20;
@@ -55,9 +53,7 @@ class Platform {
     }
 }
 
-const player = new Player();
-const platform = new Platform();
-platform.draw();
+
 const keys = {
     right:{
         pressed: false
@@ -74,26 +70,72 @@ const keys = {
     }
 }
 
+const player = new Player();
+const platforms = [];
+const init = () => {
+    let x = 200;
+    let y = 600;
+    for (let i=0; i<50; i++){
+        x += 300;
+        y -= 100;
+        platforms.push(new Platform({x, y}));
+    }
+
+}
+
 const animate = () => {
     const animationFrame = requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = "yellow";
+    c.fillRect(0, canvas.height - 30, canvas.width, 30);
 
     player.update();
 
-    if(keys.right.pressed){
+    if(keys.right.pressed && player.position.x <= canvas.width / 2){
         player.velocity.x = 5;
-    }else if(keys.left.pressed){
+    }else if(keys.left.pressed && player.position.x >= 100){
         player.velocity.x = -5;
     }else{
         player.velocity.x = 0;
+        if (keys.right.pressed){
+            platforms.forEach(platform => {
+                platform.position.x -= 5;
+            })
+        }else if(keys.left.pressed){
+            platforms.forEach(platform => {
+                platform.position.x += 5;
+            })
+        }
     }
+
+    // jumping 
     if(keys.up.pressed && keys.up.count <= 1){
         player.velocity.y = -16;
         keys.up.count++;
     }
+
+    //platform detection
+    //detection to disallow multiple jumping PART 1
+    platforms.forEach(platform => {
+        platform.draw();
+
+        if(player.position.y + player.height / 2 <= platform.position.y 
+        && player.position.y + player.height / 2+ player.velocity.y >= platform.position.y
+        && (player.position.x > platform.position.x && player.position.x < platform.position.x + platform.width)){
+            player.velocity.y = 0;
+            keys.up.count = 0;
+        }
+        //detection to disallow multiple jumping PART 2
+        if(player.position.y == canvas.height - player.height / 2 - player.velocity.y - 30){
+            keys.up.count = 0;
+        }
+    })
     
+
+    console.log(keys.up.count);
 }
 
+init();
 animate();
 
 addEventListener("keydown", ({key}) => {
